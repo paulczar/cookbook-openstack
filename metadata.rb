420bbe2958d248a51e038eb528e777c483e0b7fb
Paul Czarkowski <paul@paulcz.net>
2014-10-04 18:02:38 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'username.taken@gmail.com'
 license 'Apache2'
 description 'Library cookbook to interact with Openstack Clouds'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 %w(build-essential apt xml).each do |ckbk|
   depends ckbk
